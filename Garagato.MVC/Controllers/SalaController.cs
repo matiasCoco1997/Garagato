0d03cda1e1deb8923299cc8836110f04200fe586
Matias Ariel Coco <cocomatias6@gmail.com>
2024-10-12 13:34:32 -0300

    update vista sala de juego terminada

---
+++ Garagato.MVC/Controllers/SalaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Garagato.MVC.Controllers;
+
+public class SalaController : Controller
+{
+    public IActionResult Juego()
+    {
+        return View("Index");
+    }
+}
